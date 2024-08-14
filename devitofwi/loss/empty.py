89a7461908c33730b791f7eb1ec1bfdd27a03e15
mrava87 <matteoravasi@gmail.com>
2024-08-14 22:45:13 +0200

    doc: added RTM example

---
+++ devitofwi/loss/empty.py
@@ -0,0 +1,29 @@
+__all__ = ["Empty",
+           ]
+
+import numpy as np
+
+
+class Empty():
+    r"""Empty Norm.
+    
+    This class implements an empty norm, where the gradient 
+    is simply represented by the data vector (and the norm
+    itself simply returns 0). To be used only to perform RTM
+    (i.e., to have an adjoint source equal to the data)
+
+    Parameters
+    ----------
+    b : :obj:`numpy.ndarray`
+        Data vector
+    
+    """
+    def __init__(self, b):
+        self.b = b
+        
+    def __call__(self, x, i):
+        # not implemented (simply return 0)
+        return 0.
+    
+    def grad(self, x, i):
+        return self.b[i]
